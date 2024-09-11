41563f1f5ced78e1f084db7bd54e41ff675e3434
Abdurrahman <free.abdo.sh@gmail.com>
2024-09-11 07:06:54 +0300

    Implementing the stack as a seperated class file

--- Implementing a scientific calculator using stack/main.cpp
+++ Implementing a scientific calculator using stack/main.cpp
@@ -2,59 +2,6 @@
 #include <iostream>
 #include "Stack.h"
 using namespace std;
-//template<class t>
-//
-//class stack{
-//	struct Item
-//	{
-//		t Value;
-//		Item* LastItemPtr;
-//	};
-//	Item *top, *current;
-//
-//public:
-//	stack() {
-//		top = NULL;
-//	}
-//
-//	void push(t newItem) {
-//		Item* newItemPtr = new Item;
-//		newItemPtr->Value = newItem;
-//		newItemPtr->LastItemPtr = top;
-//		top = newItemPtr;
-//
-//	}
-//	
-//	void is_Empty() {
-//		return top == NULL;
-//	}
-//
-//	void pop() {
-//		if (!is_Empty()) {
-//			Item* temp = top;
-//			top = top->LastItemPtr;
-//			delete temp;
-//		}
-//		else {
-//			cout << "Stack is empty";
-//		}
-//
-//	}
-//
-//	void display() {
-//		current = top;
-//		cout << "[ ";
-//
-//		while (current != NULL) {
-//			cout << current->Value << " ";
-//			current = current->LastItemPtr;
-//		}
-//
-//		cout << "] ";
-//	}
-//
-//};
-//
 
 int main()
 {
